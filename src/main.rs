8834c307393b855cb43cd01fbe166f581f6351dd
Simon Ninon <simon.ninon@gmail.com>
2023-04-22 00:34:28 -0700

    basic repl loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io::{self, Write};
+use std::process::exit;
+
+fn print_prompt() {
+    print!("dummy-db> ");
+    io::stdout().flush().unwrap_or_default();
+}
+
+fn read_input() -> String {
+    let mut buffer = String::new();
+
+    io::stdin()
+        .read_line(&mut buffer)
+        .unwrap_or_default();
+
+    buffer.trim().to_string()
+}
+
+fn eval_command(command: &str) {
+    match command {
+        ".exit" => exit(0),
+        _ => println!("Unrecognized command '{command}'")
+    }
+}
+
+fn repl_loop() {
+    loop {
+        print_prompt();
+
+        let command = read_input();
+        eval_command(&command);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    repl_loop();
 }
