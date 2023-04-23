fcaeaeb286a3e62066a59380e24e0106525e1ec7
Simon Ninon <simon.ninon@gmail.com>
2023-04-23 00:02:58 -0700

    reorganize with modules

---
+++ src/core/interface/cli.rs
@@ -0,0 +1,33 @@
+use std::io::{self, Write};
+use std::process::exit;
+
+fn print_prompt() {
+    print!("dummy-db> ");
+    io::stdout().flush().unwrap_or_default();
+}
+
+fn read_input() -> String {
+    let mut buffer = String::new();
+
+    io::stdin()
+        .read_line(&mut buffer)
+        .unwrap_or_default();
+
+    buffer.trim().to_string()
+}
+
+fn eval_command(command: &str) {
+    match command {
+        ".exit" => exit(0),
+        _ => println!("Unrecognized command '{command}'")
+    }
+}
+
+pub fn repl_loop() {
+    loop {
+        print_prompt();
+
+        let command = read_input();
+        eval_command(&command);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,5 @@
-use std::io::{self, Write};
-use std::process::exit;
-
-fn print_prompt() {
-    print!("dummy-db> ");
-    io::stdout().flush().unwrap_or_default();
-}
-
-fn read_input() -> String {
-    let mut buffer = String::new();
-
-    io::stdin()
-        .read_line(&mut buffer)
-        .unwrap_or_default();
-
-    buffer.trim().to_string()
-}
-
-fn eval_command(command: &str) {
-    match command {
-        ".exit" => exit(0),
-        _ => println!("Unrecognized command '{command}'")
-    }
-}
-
-fn repl_loop() {
-    loop {
-        print_prompt();
-
-        let command = read_input();
-        eval_command(&command);
-    }
-}
+use dummy_db::core::interface::cli;
 
 fn main() {
-    repl_loop();
+    cli::repl_loop();
 }
